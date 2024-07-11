3ae0d5ce77b8f6e7c8f837e1fe508097edd488e2
Richard Wang <wangleihd@gmail.com>
2024-07-11 23:25:40 +0800

    Fixed banner show bug.

--- src/components/Common/Banner/index.tsx
+++ src/components/Common/Banner/index.tsx
@@ -19,7 +19,7 @@ export default function BannerInIndex({ banners }: { banners: Banner[] }) {
 				/>
 			</div>
 			{/* 文字和按钮 */}
-			<div className="md:w-2/5 p-6 md:p-18 flex flex-col z-10">
+			<div className="md:w-2/5 p-4 lg:p-24 flex flex-col z-10">
 				<h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">
 					Upgrade Your Wardrobe With Our Collection
 				</h1>
@@ -27,7 +27,7 @@ export default function BannerInIndex({ banners }: { banners: Banner[] }) {
 					Discover the latest trends and styles to enhance your fashion sense. Browse through our exclusive collection and find the perfect fit for every occasion.
 				</p>
 				<div className="space-x-4">
-					<button className="text-white w-full md:w-1/3 bg-fta-primary-400 hover:bg-fta-primary-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 text-center">
+					<button className="text-white w-full lg:w-1/3 bg-fta-primary-400 hover:bg-fta-primary-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 text-center">
 						Sign Up
 					</button>
 				</div>
